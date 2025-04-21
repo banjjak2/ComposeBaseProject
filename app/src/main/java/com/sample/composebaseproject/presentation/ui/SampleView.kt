73e99a5b5512aa3d31e43e129091bc50dd9d9dcf
wooki <interr0bang@naver.com>
2025-04-22 01:18:48 +0900

    SampleView 파일 추가

---
+++ app/src/main/java/com/sample/composebaseproject/presentation/ui/SampleView.kt
@@ -0,0 +1,15 @@
+package com.sample.composebaseproject.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SampleView() {
+
+}
+
+@Composable
+@Preview
+fun SampleViewPreview() {
+    SampleView()
+}
